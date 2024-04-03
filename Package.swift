de43ac81589f72935136f958b3d8cf0b5c599cd1
Mohamed Bendnaiba <mohamed.bendnaiba@vosker.com>
2024-04-03 13:50:52 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "SegmentFirebase",
+    name: "JournifyFirebase",
     platforms: [
         .macOS("10.15"),
         .iOS("13.0"),
@@ -14,29 +14,29 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "SegmentFirebase",
-            targets: ["SegmentFirebase"]),
+            name: "JournifyFirebase",
+            targets: ["JournifyFirebase"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(
-            name: "Segment",
-            url: "https://github.com/segmentio/analytics-swift.git",
-            from: "1.4.7"
+            name: "Journify",
+            url: "https://github.com/journifyio/journify-ios-sdk.git",
+            from: "0.0.6"
         ),
 		.package(
 			name: "Firebase",
 			url: "https://github.com/firebase/firebase-ios-sdk",
-			from: "10.21.0"
+			from: "10.23.1"
 		)
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "SegmentFirebase",
-            dependencies: ["Segment",
+            name: "JournifyFirebase",
+            dependencies: ["Journify",
                             .product(
                                 name: "FirebaseAnalytics",
                                 package: "Firebase")
